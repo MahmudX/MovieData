a4c9e34823b1611666d4917400f29573acc4688c
Mahmud <Mahmud@LUMINOUS>
2019-01-29 01:06:04 +0600

    Initial Added Logos Added API Customized API Designed UI Modified UI Added Function

--- MovieData/MainPage.xaml.cs
+++ MovieData/MainPage.xaml.cs
@@ -11,20 +11,56 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace MovieData
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        bool imageMagnifierButtonValidation = false;
         public MainPage()
         {
             this.InitializeComponent();
+            imageMagnifier.IsEnabled = false;
+        }
+        
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                RootObject myMovieData = await OMDbAPI.GetIMDbData(searchBox.Text.ToString());
+                Warning.Text = "";
+                movieName.Text = myMovieData.Title;
+                movieGenre.Text = myMovieData.Genre;
+                movieRated.Text = myMovieData.Rated;
+                movieRating.Text = myMovieData.imdbRating;
+                movieYear.Text = myMovieData.Year;
+                movieDirector.Text = myMovieData.Director;
+                movieBoxOffice.Text = myMovieData.BoxOffice;
+                moviePoster.Source = new BitmapImage(new Uri(myMovieData.Poster, UriKind.Absolute));
+                imageMagnifierButtonValidation = true;
+                imageMagnifier.IsEnabled = true;
+                imageMagnifier.Content = "Zoom IO";
+            }
+            catch (Exception)
+            {
+                Warning.Text = "Please enter a valid movie name.";
+                imageMagnifierButtonValidation = false;
+            }            
+        }
+
+        private void imageMagnifier_Click(object sender, RoutedEventArgs e)
+        {
+            if (moviePoster.Height == 500)
+            {
+                moviePoster.Height = 200;
+            }
+            else if (moviePoster.Height == 200)
+            {
+                moviePoster.Height = 500;
+            }
+            
         }
     }
 }
---
+++ MovieData/OMDbAPI.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+
+namespace MovieData
+{
+    public class OMDbAPI
+    {
+        public async static Task<RootObject> GetIMDbData(string movieTitle)
+        {
+            var http = new HttpClient();
+            var response = await http.GetAsync(String.Format("http://www.omdbapi.com/?apikey=e2d74f62&t={0}", movieTitle));
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (RootObject)serializer.ReadObject(ms);
+
+            return data;
+        }
+    }
+
+    [DataContract]
+
+    public class Rating
+    {
+        [DataMember]
+        public string Source { get; set; }
+
+        [DataMember]
+        public string Value { get; set; }
+    }
+
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string Year { get; set; }
+
+        [DataMember]
+        public string Rated { get; set; }
+
+        [DataMember]
+        public string Released { get; set; }
+
+        [DataMember]
+        public string Runtime { get; set; }
+
+        [DataMember]
+        public string Genre { get; set; }
+
+        [DataMember]
+        public string Director { get; set; }
+
+        [DataMember]
+        public string Writer { get; set; }
+
+        [DataMember]
+        public string Actors { get; set; }
+
+        [DataMember]
+        public string Plot { get; set; }
+
+        [DataMember]
+        public string Language { get; set; }
+
+        [DataMember]
+        public string Country { get; set; }
+
+        [DataMember]
+        public string Awards { get; set; }
+
+        [DataMember]
+        public string Poster { get; set; }
+
+        [DataMember]
+        public List<Rating> Ratings { get; set; }
+
+        [DataMember]
+        public string Metascore { get; set; }
+
+        [DataMember]
+        public string imdbRating { get; set; }
+
+        [DataMember]
+        public string imdbVotes { get; set; }
+
+        [DataMember]
+        public string imdbID { get; set; }
+
+        [DataMember]
+        public string Type { get; set; }
+
+        [DataMember]
+        public string DVD { get; set; }
+
+        [DataMember]
+        public string BoxOffice { get; set; }
+
+        [DataMember]
+        public string Production { get; set; }
+
+        [DataMember]
+        public string Website { get; set; }
+
+        [DataMember]
+        public string Response { get; set; }
+    }
+}
